b55b8d58df1173d65febe579c2239cb0d8121146
Amanda <amanda@depaula.xyz>
2025-03-15 16:24:15 -0400

    switch to chrono library and update items data and buttons

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createTheme, ThemeProvider } from "@mui/material";
 
-createRoot(document.getElementById('root')!).render(
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#6b654b",
+    },
+    secondary: {
+      main: "#d4df9e",
+    },
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
--- src/timeline.tsx
+++ src/timeline.tsx
@@ -1,31 +1,78 @@
-import { Chrono } from "react-chrono";
-import { itemsData } from "./itemsData";
+import { itemsData, VehicleCardData } from "./itemsData";
+import styled from "styled-components";
 
 import {
-  DirectionsBoatFilledSharp,
-  DirectionsRunSharp,
-  Flight,
-  TimeToLeave,
-  Train,
-} from "@mui/icons-material";
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { useState } from "react";
+import { Button, ButtonGroup, Typography } from "@mui/material";
+
+const SplitContent = ({ data }: { data: VehicleCardData }) => {
+  enum ContentType {
+    Allegorical,
+    Literal,
+  }
+  const [which, setWhich] = useState<ContentType>(ContentType.Literal);
+
+  return (
+    <SplitContainer>
+      <ButtonGroup
+        style={{ marginBottom: "1em" }}
+        variant="contained"
+        aria-label="Basic button group"
+      >
+        <Button
+          className={which === ContentType.Literal ? "Mui-focusVisible" : ""}
+          onClick={() => {
+            setWhich(ContentType.Literal);
+          }}
+        >
+          Literal
+        </Button>
+        <Button
+          className={
+            which === ContentType.Allegorical ? "Mui-focusVisible" : ""
+          }
+          onClick={() => {
+            setWhich(ContentType.Allegorical);
+          }}
+        >
+          Allegorical
+        </Button>
+      </ButtonGroup>
+      <div>
+        {which === ContentType.Allegorical && <>{data.allegoricalContent}</>}
+        {which === ContentType.Literal && <>{data.literalContent}</>}
+      </div>
+    </SplitContainer>
+  );
+};
+
+const SplitContainer = styled.div`
+  width: 100%;
+`;
 
 const Timeline = () => {
   return (
-    <Chrono
-      items={itemsData}
-      mode="VERTICAL_ALTERNATING"
-      scrollable
-      toolbarPosition="bottom"
-      highlightCardsOnHover="true"
-    >
-      <div className="chrono-icons">
-        <TimeToLeave sx={{ bgcolor: "white" }} />
-        <Train sx={{ bgcolor: "white" }} />
-        <Flight sx={{ bgcolor: "white" }} />
-        <DirectionsBoatFilledSharp sx={{ bgcolor: "white" }} />
-        <DirectionsRunSharp sx={{ bgcolor: "white" }} />
-      </div>
-    </Chrono>
+    <>
+      <VerticalTimeline>
+        {itemsData.map((d, i) => (
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            icon={d.icon}
+            contentStyle={{ background: "#D4DF9E", color: "#12100E" }}
+            contentArrowStyle={{ borderRight: "7px solid #D4DF9E" }}
+            key={i}
+          >
+            <Typography variant="h4">{d.title}</Typography>
+            <SplitContent data={d} />
+          </VerticalTimelineElement>
+        ))}
+      </VerticalTimeline>
+      ;
+    </>
   );
 };
 
